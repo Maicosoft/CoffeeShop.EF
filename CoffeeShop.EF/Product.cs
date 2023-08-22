1c3e2b3a1e1e67f8ad72502f0609d89628e5fe24
Maico Silva <maico1978@outlook.com>
2023-08-22 09:39:04 +0200

    created DB with EF and sqlite

---
+++ CoffeeShop.EF/Product.cs
@@ -0,0 +1,7 @@
+﻿namespace CoffeeShop.EF;
+
+internal class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
---
+++ CoffeeShop.EF/ProductsContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeShop.EF;
+
+internal class ProductsContext: DbContext
+{
+    public DbSet<Product> products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+        optionsBuilder.UseSqlite($"Data source = product.db");
+}
